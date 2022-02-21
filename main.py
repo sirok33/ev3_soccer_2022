9dfd7980bc9d2d88bcbe617817b764588d93fcbe
sirok33 <cenen.spb@gmail.com>
2022-02-21 20:01:41 +0300

    добавил нового кота

--- main.py
+++ main.py
@@ -6,74 +6,100 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
-
+from pybricks.iodevices import I2CDevice
+from pybricks.iodevices import Ev3devSensor
+import math
 import time
 # Create your objects here.
 ev3 = EV3Brick()
+compass = I2CDevice(Port.S1, 0x01)
+seeker = Ev3devSensor(Port.S2)
 motorb = Motor(Port.B)
 motorc = Motor(Port.C)
-sonic = UltrasonicSensor(Port.S2)
-color1 = ColorSensor(Port.S1)
-color2 = ColorSensor(Port.S3)
-gyrok = GyroSensor(Port.S4)
-
+motora = Motor(Port.A)
+time1 = time.time()
+color1 = ColorSensor(Port.S3)
 # Write your program here.
-
-gyric = gyrok.angle()
-kp = 1.2
+ev3.speaker.beep()
+kc = 1.1
+ks = 15
 v = 50
-u = 0
 err = 0
-count = 0
-ref1 = color1.reflection() 
-ref2 = color2.reflection()
-t1 = time.time()
+dir = 0
+see = 0
 t2 = 0
-rast = sonic.distance()
-stage = 0
-gyrok.reset_angle(0)
-wait(300)
-motorb.dc(50)
-motorc.dc(50)
-wait(1500)
-
+ucom = 0
+result = compass.read(reg = 0x42, length = 1)
+alpha = int(result[0]) * 2
+stage = 1
+col = False
 while True:
-    motorb.dc(0)
-    motorc.dc(0)  
-    rast = sonic.distance()
-    if stage == 0 and rast<150:
-        
-        wait(6000)
-        motorb.dc(100)
-        motorc.dc(100)
-        wait(2000)
-        motorb.dc(0)
-        motorc.dc(0)
-        while rast > 300:
-            motorb.dc(0)
-            motorc.dc(0)
-        stage = 1
-             
-    t2 = time.time() - t1
-    t1 = time.time() - t1
-    ref1 = color1.reflection() 
-    ref2 = color2.reflection()
-    err = ref1 - ref2
-    u = kp * err
-    motorb.dc(v+u)
-    motorc.dc(v-u)
-    if ref1 < 10 and ref2 < 10 :
-        count += 1
+    amb = color1.ambient()
+    if amb>25 and see > 120 :
+        col = True
         ev3.speaker.beep()
-        wait(100)
-    ev3.screen.clear()
-    ev3.screen.print(t2)
-    ev3.screen.print(t1)
-    ev3.screen.print()
+    else:
+        col=False
+    t1 = time.time() - time1
+    result = compass.read(reg = 0x42, length = 1)
+    sek_res = seeker.read("AC-ALL")
+    dir = sek_res[0]
+    see = 0
+    see = sek_res[1]+sek_res[2]+sek_res[3]+sek_res[4]+sek_res[5]
 
+    compas = int(result[0]) * 2     
+    err = alpha - compas
+    er = err / 180
+
+    if stage == 1:
+        t2 = time.time() - time1
+        u = (dir - 5) * ks
+    
+    
+        if dir == 5 and see  > 120 and col ==True:
+            ev3.speaker.beep()
+            stage = 2
+    #if err > 100:
+       # while err > 90:
+        #    motorb.dc(60)
+        #   motorc.dc(45)
+    
+    elif stage == 2:
+        u = ucom
+        t2 = time.time()- t1
+        if er > 0:
+            er = math.floor(er)
+        else:
+            er = math.ceil(er)
+        ucom = kc*(err - er*360)
+        if t1 - t2 > 2 :
+            stage = 3
+        if abs(ucom)>20:
+            if ucom>0:
+                ucom=20
+            else:
+                ucom = -20
+        
+        
+        if stage == 3:
+            motora.dc(100)
+            wait(200)
+            motora.dc(-100)
+            wait(200)
+            
+
+        if dir != 5 and see  < 120:
+            stage = 1
+            ev3.speaker.beep()
 
     
-
+    motorb.dc(v+u)
+    motorc.dc(v-u)
+    ev3.screen.clear()
+    ev3.screen.print(dir)
+    ev3.screen.print(see)
+    ev3.screen.print(t1)
+    ev3.screen.print(amb)
+    wait(10)  
 
 
